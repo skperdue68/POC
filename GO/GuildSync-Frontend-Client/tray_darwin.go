c10da376ff47f121af19557591c1d1f8f201bf6c
Stephanie Perdue <skperdue68@gmail.com>
2026-06-22 04:31:53 -0500

    Add cross-platform tray build fixes
    - Add Linux build dependency for getlantern/systray support
    - Install libayatana-appindicator3-dev in Linux GitHub Actions build
    - Replace macOS systray usage to avoid Wails AppDelegate conflicts
    - Add native macOS menu-bar tray controller
    - Fix macOS duplicate Objective-C symbols by compiling tray controller once
    - Preserve Linux tray behavior
    - Leave Windows behavior unchanged

--- GO/GuildSync-Frontend-Client/tray_darwin.go
+++ GO/GuildSync-Frontend-Client/tray_darwin.go
@@ -3,89 +3,9 @@
 package main
 
 /*
-#cgo CFLAGS: -x objective-c
 #cgo LDFLAGS: -framework Cocoa
-#import <Cocoa/Cocoa.h>
-
-extern void guildsyncTrayShow(void);
-extern void guildsyncTrayHide(void);
-extern void guildsyncTrayQuit(void);
-
-@interface GuildSyncTrayController : NSObject
-@property (strong) NSStatusItem *statusItem;
-@end
-
-@implementation GuildSyncTrayController
-
-- (void)showGuildSync:(id)sender {
-    guildsyncTrayShow();
-}
-
-- (void)hideGuildSync:(id)sender {
-    guildsyncTrayHide();
-}
-
-- (void)quitGuildSync:(id)sender {
-    guildsyncTrayQuit();
-}
-
-@end
-
-static GuildSyncTrayController *guildsyncTrayController = nil;
-
-static void guildsyncCreateTrayOnMainThread(void) {
-    if (guildsyncTrayController != nil) {
-        return;
-    }
-
-    guildsyncTrayController = [[GuildSyncTrayController alloc] init];
-    guildsyncTrayController.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-
-    if (guildsyncTrayController.statusItem.button != nil) {
-        guildsyncTrayController.statusItem.button.title = @"GuildSync";
-        guildsyncTrayController.statusItem.button.toolTip = @"GuildSync is running";
-    }
-
-    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"GuildSync"];
-
-    NSMenuItem *showItem = [[NSMenuItem alloc] initWithTitle:@"Show" action:@selector(showGuildSync:) keyEquivalent:@""];
-    [showItem setTarget:guildsyncTrayController];
-    [menu addItem:showItem];
-
-    NSMenuItem *hideItem = [[NSMenuItem alloc] initWithTitle:@"Hide" action:@selector(hideGuildSync:) keyEquivalent:@""];
-    [hideItem setTarget:guildsyncTrayController];
-    [menu addItem:hideItem];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    NSMenuItem *quitItem = [[NSMenuItem alloc] initWithTitle:@"Quit GuildSync" action:@selector(quitGuildSync:) keyEquivalent:@"q"];
-    [quitItem setTarget:guildsyncTrayController];
-    [menu addItem:quitItem];
-
-    guildsyncTrayController.statusItem.menu = menu;
-}
-
-static void guildsyncRemoveTrayOnMainThread(void) {
-    if (guildsyncTrayController == nil) {
-        return;
-    }
-
-    [[NSStatusBar systemStatusBar] removeStatusItem:guildsyncTrayController.statusItem];
-    guildsyncTrayController.statusItem = nil;
-    guildsyncTrayController = nil;
-}
-
-static void guildsyncCreateTray(void) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        guildsyncCreateTrayOnMainThread();
-    });
-}
-
-static void guildsyncRemoveTray(void) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        guildsyncRemoveTrayOnMainThread();
-    });
-}
+void guildsyncCreateTray(void);
+void guildsyncRemoveTray(void);
 */
 import "C"
 
